3ee95772e638cb6c02484884fad26cd5b680a4ea
Daniel Waiguru <danielirungu91@gmail.com>
2020-05-28 16:22:28 +0300

    added user profile logic

---
+++ src/handlers/Profile.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Handlers;
+use Components\Auth;
+use Components\Template;
+
+class Profile extends Handler
+{
+    public function handle(): string
+    {
+        if (!Auth::userIsAuthenticated()) {
+            return (new Login)->handle();
+        }
+        return (new Template('profile'))->render();
+    }
+} 
