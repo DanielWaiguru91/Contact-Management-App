e59829a18bd92abb02a06efee1f5aa920a31f68d
Daniel Waiguru <danielirungu91@gmail.com>
2020-05-28 16:31:20 +0300

    added logout logic

---
+++ src/handlers/Logout.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Handlers;
+use Components\Auth;
+
+
+class Logout extends Handler{
+    public function handle(): string{
+        
+        Auth::logout();
+        $this->requestRedirect('/');
+        return '';
+    }
+}
